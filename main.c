470b26258ead8c46b4ab7d3d24935021d6bb53be
Manoel Almeida <tmanoel64x@gmail.com>
2019-12-26 17:07:10 -0300

    Added background

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "tiles/keyboard.c"
+#include "tiles/keyboardmap.c"
 
 void main() {
-    printf("Hello, world!");
+    
+    set_bkg_data(0, 103, keyboardTiles);
+    set_bkg_tiles(0, 0, 20, 18, keyboardMap);
+
+    SHOW_BKG;
+    DISPLAY_ON;
 }
