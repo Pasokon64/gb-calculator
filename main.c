b516a167c0b825650c64b17cbf276fda2ae87baa
Manoel Almeida <tmanoel64x@gmail.com>
2020-01-03 20:23:34 -0300

    Display setup

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "tiles/cursor.c"
 #include "tiles/keyboard.c"
 #include "tiles/keyboardmap.c"
 
+int pow(int, int);
+
 void keyboard();
+void updateDisplay();
 void changeCursorDelay();
 void changeCursorPosition();
 void changeCursorSprite(int);
 
-int currentCursorSpriteIndex = 0;
+void toDisplay(unsigned int);
+
+char display[15] = "100000";
+unsigned char displayDigits[24];
+
 unsigned int lastTime = 0;
+int currentCursorSpriteIndex = 0;
 
 int cursorLocations[][4][2] = {
     { { 40, 48 }, { 64, 48 }, { 88, 48 }, { 120, 48 } },
@@ -46,14 +56,42 @@ void main() {
     SHOW_SPRITES;
     DISPLAY_ON;
 
+    toDisplay((unsigned int)0);
+
     while (1) {
 
+        updateDisplay();
         keyboard();
         changeCursorPosition();
         
         changeCursorDelay(&lastTime);
         delay(100);
     }
+}
+
+void updateDisplay() {
+
+    size_t i;
+
+    for (i = 0; i < 12; i++) {
+        
+        int cPos = (strlen(display) - i - 1);
+
+        if (cPos > 0) {
+        
+            int numberTile = (display[cPos] - 48) * 2 + 80;
+
+            displayDigits[11 - i] = numberTile;
+            displayDigits[23 - i] = numberTile + 1;
+        }
+        else {
+
+            displayDigits[11 - i] = 0;
+            displayDigits[23 - i] = 0;
+        }
+    }
+
+    set_bkg_tiles(4, 1, 12, 2, displayDigits);
 }
 
 void keyboard() {
@@ -117,4 +155,47 @@ void changeCursorSprite(int index) {
         set_sprite_tile(0, 4);
         set_sprite_tile(1, 6);
     }
+}
+
+void toDisplay(unsigned int value) {
+
+    size_t i;
+    int numDigits = 1;
+    unsigned int copy = abs(value);
+    strcpy(display, "");
+
+    while (copy >= 10)   {
+        
+        copy /= 10;
+        numDigits++;
+    }
+
+    for (i = (numDigits - 1); i > 0; i--) {
+        
+        char temp[0];
+        int digit = value / pow(10, i);
+        value = value % pow(10, i);
+
+        temp[0] = (char)digit + 48;
+
+        strcat(display, temp);
+    }
+}
+
+int pow(int base, int expo) {
+
+    int i;
+    int result = base;
+
+    if (expo == 0)  {
+        
+        return 1;
+    }
+
+    for (i = 1; i < expo; i++) {
+    
+        result *= base;
+    }
+
+    return result;
 }
