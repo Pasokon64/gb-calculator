f42b0b55b4b55cb16e0889079d9a73bb94257453
Manoel Almeida <tmanoel64x@gmail.com>
2020-01-06 20:52:07 -0300

    Fix calculator display

--- main.c
+++ main.c
@@ -3,41 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "math.c"
 #include "tiles/cursor.c"
 #include "tiles/keyboard.c"
 #include "tiles/keyboardmap.c"
 
-int pow(int, int);
-
 void keyboard();
+void action(char);
 void updateDisplay();
+void addToDisplay(char);
 void changeCursorDelay();
 void changeCursorPosition();
 void changeCursorSprite(int);
 
-void toDisplay(unsigned int);
-
-char display[15] = "100000";
-unsigned char displayDigits[24];
+char display[12] = "0";
 
 unsigned int lastTime = 0;
 int currentCursorSpriteIndex = 0;
 
-int cursorLocations[][4][2] = {
-    { { 40, 48 }, { 64, 48 }, { 88, 48 }, { 120, 48 } },
-    { { 40, 72 }, { 64, 72 }, { 88, 72 }, { 120, 72 } },
-    { { 40, 96 }, { 64, 96 }, { 88, 96 }, { 120, 96 } },
-    { { 40, 120 }, { 64, 120 }, { 88, 120 }, { 120, 120 } },
-    { { 40, 144 }, { 64, 144 }, { 88, 144 }, { 120, 144 } },
+int cursorLocations[][4][2] =
+    {
+        {{40, 48}, {64, 48}, {88, 48}, {120, 48}},
+        {{40, 72}, {64, 72}, {88, 72}, {120, 72}},
+        {{40, 96}, {64, 96}, {88, 96}, {120, 96}},
+        {{40, 120}, {64, 120}, {88, 120}, {120, 120}},
+        {{40, 144}, {64, 144}, {88, 144}, {120, 144}},
 };
+
+char keyboardOptions[][4] =
+    {
+        {' ', 'c', '<', '/'},
+        {'7', '8', '9', '*'},
+        {'4', '5', '6', '-'},
+        {'1', '2', '3', '+'},
+        {'%', '0', ',', '='}};
 
 int cursorX = 0;
 int cursorY = 0;
 int maxPosX = 0;
 int maxPosY = 0;
 
-void main() {
-    
+int keydown = 0;
+
+void main()
+{
     maxPosX = sizeof(cursorLocations[0]) / sizeof(cursorLocations[0][0]) - 1;
     maxPosY = sizeof(cursorLocations) / sizeof(cursorLocations[0]) - 1;
 
@@ -56,66 +65,112 @@ void main() {
     SHOW_SPRITES;
     DISPLAY_ON;
 
-    toDisplay((unsigned int)0);
-
-    while (1) {
-
-        updateDisplay();
+    while (1)
+    {
         keyboard();
+        updateDisplay();
         changeCursorPosition();
-        
+
         changeCursorDelay(&lastTime);
-        delay(100);
     }
 }
 
 void updateDisplay() {
 
-    size_t i;
+    int i;
+    char displayMap[24];
 
-    for (i = 0; i < 12; i++) {
-        
-        int cPos = (strlen(display) - i - 1);
-
-        if (cPos > 0) {
-        
-            int numberTile = (display[cPos] - 48) * 2 + 80;
-
-            displayDigits[11 - i] = numberTile;
-            displayDigits[23 - i] = numberTile + 1;
+    for (i = 0; i < 12; i++)
+    {
+        if (display[i] != '\0')
+        {
+            int c = 80 + (((int)display[i] - 48) * 2);
+            displayMap[i] = c; 
+            displayMap[12 + i] = c + 1;
         }
-        else {
-
-            displayDigits[11 - i] = 0;
-            displayDigits[23 - i] = 0;
+        else 
+        {
+            displayMap[i] = 0;
+            displayMap[12 + i] = 0;
         }
     }
 
-    set_bkg_tiles(4, 1, 12, 2, displayDigits);
+    set_bkg_tiles(4, 1, 12, 2, displayMap);
 }
 
-void keyboard() {
+void keyboard()
+{
+    if (keydown)
+    {
+        waitpadup();
+        keydown = 0;
+    }
 
-    switch (joypad()) {
-        case J_UP:
-            cursorY = cursorY > 1 ? cursorY - 1 : cursorY;
-            break;
-        case J_DOWN:
-            cursorY = cursorY < maxPosY ? cursorY + 1 : cursorY;
-            break;
-        case J_RIGHT:
-            cursorX = cursorX < maxPosX ? cursorX + 1 : cursorX;
-            break;
-        case J_LEFT:
-            cursorX = cursorX > 1 ? cursorX - 1 : cursorX;
-            break;
-        default:
-            break;
+    switch (joypad())
+    {
+    case J_UP:
+        cursorY = cursorY > 1 ? cursorY - 1 : cursorY;
+        keydown = 1;
+        break;
+    case J_DOWN:
+        cursorY = cursorY < maxPosY ? cursorY + 1 : cursorY;
+        keydown = 1;
+        break;
+    case J_RIGHT:
+        cursorX = cursorX < maxPosX ? cursorX + 1 : cursorX;
+        keydown = 1;
+        break;
+    case J_LEFT:
+        cursorX = cursorX > 1 ? cursorX - 1 : cursorX;
+        keydown = 1;
+        break;
+    case J_A:
+        action(keyboardOptions[cursorY][cursorX]);
+        keydown = 1;
+        break;
+    default:
+        break;
     }
 }
 
-void changeCursorPosition() {
+void action(char option)
+{
+    switch (option)
+    {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        addToDisplay(option);
+        break;
+    default:
+        break;
+    }
+}
 
+void addToDisplay(char option)
+{
+    if (strlen(display) < 12)
+    {
+        if (display[0] == '0' && display[1] != ',')
+        {
+            display[0] = option;
+        }
+        else
+        {
+            display[strlen(display)] = option;
+        }
+    }
+}
+
+void changeCursorPosition()
+{
     int positionX = cursorLocations[cursorY][cursorX][0];
     int positionY = cursorLocations[cursorY][cursorX][1];
 
@@ -123,18 +178,18 @@ void changeCursorPosition() {
     move_sprite(1, positionX + 8, positionY);
 }
 
-void changeCursorDelay() {
-
+void changeCursorDelay()
+{
     unsigned int currentTime = clock();
 
-    if ((currentTime - lastTime) >= 20) {
-
-        if (currentCursorSpriteIndex == 0) {
-    
+    if ((currentTime - lastTime) >= 20)
+    {
+        if (currentCursorSpriteIndex == 0)
+        {
             currentCursorSpriteIndex = 1;
         }
-        else {
-
+        else
+        {
             currentCursorSpriteIndex = 0;
         }
 
@@ -143,59 +198,16 @@ void changeCursorDelay() {
     }
 }
 
-void changeCursorSprite(int index) {
-
-    if (index == 0) {
-        
+void changeCursorSprite(int index)
+{
+    if (index == 0)
+    {
         set_sprite_tile(0, 0);
         set_sprite_tile(1, 2);
     }
-    else {
-
+    else
+    {
         set_sprite_tile(0, 4);
         set_sprite_tile(1, 6);
     }
-}
-
-void toDisplay(unsigned int value) {
-
-    size_t i;
-    int numDigits = 1;
-    unsigned int copy = abs(value);
-    strcpy(display, "");
-
-    while (copy >= 10)   {
-        
-        copy /= 10;
-        numDigits++;
-    }
-
-    for (i = (numDigits - 1); i > 0; i--) {
-        
-        char temp[0];
-        int digit = value / pow(10, i);
-        value = value % pow(10, i);
-
-        temp[0] = (char)digit + 48;
-
-        strcat(display, temp);
-    }
-}
-
-int pow(int base, int expo) {
-
-    int i;
-    int result = base;
-
-    if (expo == 0)  {
-        
-        return 1;
-    }
-
-    for (i = 1; i < expo; i++) {
-    
-        result *= base;
-    }
-
-    return result;
 }
